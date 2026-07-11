c223f94b9b96a3a1ee20fd410b8658ea21ca2bc7
HenryCast20 <castellanoshenry20@gmail.com>
2026-07-11 15:08:44 -0400

    Update users.js added bcrypt

--- server/models/users.js
+++ server/models/users.js
@@ -1,33 +1,53 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-    lowercase: true
-  },
-  password: {
-    type: String,
-    required: true
-  },
-  accountType: {
-    type: String,
-    required: true,
-    enum: ['Student', 'Faculty', 'Employee'], // Restricts input to only these choices
-    default: 'Student'
-  },
-  isVerified: {
-    type: Boolean,
-    default: false
-  }
-}, { timestamps: true });
+
+    name:{
+        type: String,
+        required: true,
+        trim: true
+    },
+
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true
+    },
+
+    password:{
+        type: String,
+        required: true
+    },
+    
+    StudentIdNumber:{
+        type: String,
+        required: true,
+        unique: true
+    },
+
+    role: {
+        type: String,
+        enum: ['student', 'faculty', 'admin'],
+        default: 'student'
+    },
+
+    activeRentals: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Device'
+    }]
+}, {timestamps: true});
+
+const bcrypt = require('bcrypt');
+
+userSchema.pre('save', async function(next) {
+    // Only hash the password if it has been modified (or is new)
+    if (!this.isModified('password')) return next();
+
+    // Hash the password with a salt round of 10
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+});
 
 module.exports = mongoose.model('User', userSchema);
