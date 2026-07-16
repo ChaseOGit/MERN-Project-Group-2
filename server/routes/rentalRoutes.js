54e82f5e9887448555a0f5d418fa2f54fc400ec3
HenryCast20 <castellanoshenry20@gmail.com>
2026-07-15 20:29:13 -0400

    Update rentalRoutes.js for transactions

--- server/routes/rentalRoutes.js
+++ server/routes/rentalRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { requireAuth, requireVerifiedEmail } = require('../middleware/authMiddleware');
 
 // Import the rental controller file you made earlier
-const { rentDevice, returnDevice } = require('../controllers/rentalController');
+const { checkoutDevice, returnDevice } = require('../controllers/loanController');
 
 // Both endpoints require a signed-in user with verified email.
 router.post('/checkout', requireAuth, requireVerifiedEmail, rentDevice);
