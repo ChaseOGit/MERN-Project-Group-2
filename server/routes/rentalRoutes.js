5c313c72da3db44139e11b7f1a188e7ad8ea5771
HenryCast20 <castellanoshenry20@gmail.com>
2026-07-16 13:25:59 -0400

    Update rentalRoutes.js added swagger annonations

--- server/routes/rentalRoutes.js
+++ server/routes/rentalRoutes.js
@@ -8,8 +8,64 @@ const { requireAuth, requireVerifiedEmail } = require('../middleware/authMiddlew
 const { rentDevice, returnDevice, getMyLoans, filterItems } = require('../controllers/rentalController');
 
 //  Define the endpoints (Protected by the JWT security checks)
+
+/**
+ * @swagger
+ * /api/rentals/checkout:
+ *   post:
+ *     summary: Check out a device
+ *     tags: [Rental]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               deviceId: { type: string }
+ *               conditionAtCheckout: { type: string }
+ *     responses:
+ *       200:
+ *         description: Device successfully checked out
+ */
 router.post('/checkout', requireAuth, requireVerifiedEmail, rentDevice);
+/**
+ * @swagger
+ * /api/rentals/return:
+ *   post:
+ *     summary: Return a device
+ *     tags: [Rental]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               deviceId: { type: string }
+ *               conditionAtReturn: { type: string }
+ *     responses:
+ *       200:
+ *         description: Device successfully returned
+ */
 router.post('/return', requireAuth, requireVerifiedEmail, returnDevice);
+/**
+ * @swagger
+ * /api/rentals/items:
+ *   get:
+ *     summary: Filter available inventory
+ *     tags: [Rental]
+ *     parameters:
+ *       - in: query
+ *         name: location
+ *         schema: { type: string }
+ *       - in: query
+ *         name: category
+ *         schema: { type: string }
+ *     responses:
+ *       200:
+ *         description: List of items matching filters
+ */
 router.get('/items', filterItems);
 
 module.exports = router;
